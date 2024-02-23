adfaea28961b1c4c6454ade8453e8b7ca3671f9f
ruhulamin-et15 <ruhulamin.et15@gmail.com>
2024-02-23 13:48:06 +0600

    update about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -18,11 +18,11 @@ const About = () => {
             time at Rangpur Polytechnic Institute, I'm brings creativity and
             innovation to every project. Committed to pushing the boundaries of
             web development/creating user-friendly and aesthetically pleasing
-            websites. In his free time, i'm enjoying delving into the latest
-            trends and technologies in web development, constantly seeking to
-            enhance my skills and expand knowledge base. With a keen eye for
-            design and functionality, poised to make a significant impact in the
-            field of web development and beyond.
+            websites. In free time, i'm enjoying delving into the latest trends
+            and technologies in web development, constantly seeking to enhance
+            my skills and expand knowledge base. With a keen eye for design and
+            functionality, poised to make a significant impact in the field of
+            web development and beyond.
           </p>
         </div>
       </div>
